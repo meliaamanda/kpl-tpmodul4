76c12f97b5f22cd3e21f9136a0716f500fdfcf0c
Amelia amanda <amelia_amanda_30rpl@student.smktelkom-mlg.sch.id>
2026-03-13 13:03:27 +0700

    menambahkan implementasi dengan table-driven

--- Program.cs
+++ Program.cs
@@ -1,1 +1,20 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using tpmodul4_103082400039;
+
+namespace tpmodul3_103082400039
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            kodePos kode = new kodePos();
+
+            Console.Write("Masukkan kelurahan: ");
+            string kelurahan = Console.ReadLine();
+
+            string hasil = kode.getKodePos(kelurahan);
+
+            Console.WriteLine("Kode Pos: " + hasil);
+        }
+    }
+}
--- obj/Debug/net10.0/tpmodul4_103082400039.AssemblyInfo.cs
+++ obj/Debug/net10.0/tpmodul4_103082400039.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("tpmodul4_103082400039")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+51bc22af65ca170bc11a741da28a8d81d6952504")]
 [assembly: System.Reflection.AssemblyProductAttribute("tpmodul4_103082400039")]
 [assembly: System.Reflection.AssemblyTitleAttribute("tpmodul4_103082400039")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
